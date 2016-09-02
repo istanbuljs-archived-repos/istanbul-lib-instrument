8192f38736b2866fa4e4eb0225db2ae6c3f2a1f8
Vladimir Agafonkin <agafonkin@gmail.com>
2016-09-03 00:30:43 +0300

    fix null checks

--- src/visitor.js
+++ src/visitor.js
@@ -91,7 +91,7 @@ class VisitState {
             return;
         }
         // else check custom node attribute set by a prior visitor
-        if (this.getAttr(path.node, 'skip-all')) {
+        if (this.getAttr(path.node, 'skip-all') !== null) {
             this.nextIgnore = n;
         }
     }
@@ -354,7 +354,7 @@ function coverSwitchCase(path) {
     const T = this.types;
     const b = this.getAttr(path.parentPath.node, 'branchName');
     /* istanbul ignore if: paranoid check */
-    if (!b) {
+    if (b === null) {
         throw new Error('Unable to get switch branch name');
     }
     const increment = this.getBranchIncrement(b, path.node.loc);
